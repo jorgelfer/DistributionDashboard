6f7ee5e29c7e396ebac89a5ea16b0c337866e050
jorgelfer <jorgeferram17@gmail.com>
2024-07-30 19:26:28 -0400

    fetching in separate file and error handling

--- src/App.js
+++ src/App.js
@@ -1,46 +1,40 @@
 import { useState, useEffect } from 'react';
-import * as d3 from 'd3';
 import Charts from './Charts/Charts';
+import Error from './UI/Error/Error';
+import { actualFetchData } from './https';
 
 export default function App() {
   const [loading, setLoading] = useState(true);
   const [data, setData] = useState([]);
+  const [error, setError] = useState();
 
    useEffect(() => {
 
     const dataURL = "http://127.0.0.1:5000/qsts/123Bus/IEEE123Master.dss";
 
-    let mounted = true;
-
-    // D3 based fetch
-    // d3.json(dataURL).then(data => {
-    //   if (mounted) {
-    //     setData(data);
-    //     setLoading(false);
-    //   }
-    // });
-
-    // native JS based fetch
-    // fetch(dataURL)
-    //   .then(response => response.json())
-    //   .then(data => {
-    //     console.log(data);
-    //     setData(data);
-    //     setLoading(false);
-    //   });
-
     // function-based JS based fetch
     async function fetchData() {
-      const response = await fetch(dataURL);
-      const data = await response.json();
+
+      try{
+
+        const data = await actualFetchData(dataURL);
+
         setData(data);
         setLoading(false);
-    }
-    fetchData();
-    
 
-    return () => mounted = false;
+        } catch (error) {
+          setError({
+            message: error.message || 'Could not fetch the data. Please try again!'
+          });
+        }
+    }
+    
+    fetchData();
   }, []); 
+
+  if (error) {
+    return <Error title="An error occurred!" message={error.message} />;
+  }
 
   return (
     <div className="container">
