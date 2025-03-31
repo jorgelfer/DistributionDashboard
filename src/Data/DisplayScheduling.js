59fcb2e43e17c66315c491e3f42e641c03778bdd
jorgelfer <jorgeferram17@gmail.com>
2025-03-31 08:54:42 -0400

    small changes

--- src/Data/DisplayScheduling.js
+++ src/Data/DisplayScheduling.js
@@ -6,7 +6,7 @@ import { fetchSchedulingData } from './https';
 export default function DisplayScheduling({payload, printRef, onSchedulingData, nodeSize}) {
   console.log("payload", payload);
   const {loading, data, error} = useFetch(fetchSchedulingData, payload);
-  console.log("payload", payload);
+  console.log("data", data);
 
   if (error) {
     return <Error title="An error occurred during Scheduling fetching!" message={error.message} />;
