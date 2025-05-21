e8429865759bda9b5f69810c444aaa446b42adfb
jorgelfer <jorgeferram17@gmail.com>
2025-05-21 10:13:00 -0500

    adding formatting on save for js files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import { useState } from "react";
-import Login from './UI/Login/Login';
-import Fetching from './Data/Fetching'
+import Login from "./UI/Login/Login";
+import Fetching from "./Data/Fetching";
 
 export default function App() {
-
   const [isCaseSubmitted, setCaseSubmitted] = useState(false);
 
   function handleSubmitted() {
@@ -15,49 +14,53 @@ export default function App() {
     inFile1: "IEEE13Nodeckt.dss",
   });
 
-  const infile1_map = { 
-    "3Bus" : "case3_unbalanced.dss",
-    "4Bus" : "4Bus-DY-reg.dss",
+  const infile1_map = {
+    "3Bus": "case3_unbalanced.dss",
+    "4Bus": "4Bus-DY-reg.dss",
     "13Bus": "IEEE13Nodeckt.dss",
     "123Bus": "IEEE123Master.dss",
-    "secondary": "Master.dss",
+    secondary: "Master.dss",
     "8500Node": "Master-unbal.dss",
     "8500Node_120": "Master-unbal.dss",
     "8500Node_1ph": "Master.dss",
     "8500Node_441": "Master.dss",
     "8500Node_510": "Master-unbal.dss",
-    "center_tapped": "center_tap_xfmr.dss",
-    };
+    center_tapped: "center_tap_xfmr.dss",
+  };
 
   function handleInputChange(identifier, value) {
-    setEnteredCase(prevCase => ({
+    setEnteredCase((prevCase) => ({
       ...prevCase,
-      [identifier]: value
-    }))
+      [identifier]: value,
+    }));
     // By default inFile1 based on networkModel
     if (identifier === "networkModel") {
-      setEnteredCase(prevCase => ({
+      setEnteredCase((prevCase) => ({
         ...prevCase,
         inFile1: infile1_map[value],
-      }))
-    } 
+      }));
+    }
   }
 
   return (
     <>
-      {!isCaseSubmitted && <main>
-        <Login
-          values={enteredCase}
-          onEnteredValues={handleInputChange}
-          onSubmitted={handleSubmitted}
-        />
-      </main>}
-      {isCaseSubmitted && <div className="container">
-        <Fetching 
-          networkModel={enteredCase.networkModel} 
-          inFile1={enteredCase.inFile1}
-        />
-      </div>}
+      {!isCaseSubmitted && (
+        <main>
+          <Login
+            values={enteredCase}
+            onEnteredValues={handleInputChange}
+            onSubmitted={handleSubmitted}
+          />
+        </main>
+      )}
+      {isCaseSubmitted && (
+        <div className="container">
+          <Fetching
+            networkModel={enteredCase.networkModel}
+            inFile1={enteredCase.inFile1}
+          />
+        </div>
+      )}
     </>
   );
-};
+}
